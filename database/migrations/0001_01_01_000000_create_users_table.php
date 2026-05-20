7f4641f64b91c4fe94e87473ba65989d45a4800d
khaledmv <khaledhossionmv@gmail.com>
2026-05-20 14:25:53 +0600

    Auth created

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,6 +16,11 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('photo')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->string('role')->default('user');
+            $table->string('status')->default('1');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
